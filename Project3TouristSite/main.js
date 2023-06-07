2a769ab6f97d61f698d2e3c2dc7df42beba78cae
Shivang Yadav <shivangyadav121@gmail.com>
2023-06-07 18:38:40 +0530

    updated home and contact page

--- Project3TouristSite/main.js
+++ Project3TouristSite/main.js
@@ -1,55 +1,55 @@
-const homebtn = document.getElementById('home');
-const homemenu = document.getElementById('homebox');
+const homebtn = document.getElementById("home");
+const homemenu = document.getElementById("homebox");
 
-homebtn.addEventListener('mouseenter', showhomebox);
-homebtn.addEventListener('mouseleave', hidehomebox);
-homemenu.addEventListener('mouseenter', showhomebox);
-homemenu.addEventListener('mouseleave', hidehomebox);
+homebtn.addEventListener("mouseenter", showhomebox);
+homebtn.addEventListener("mouseleave", hidehomebox);
+homemenu.addEventListener("mouseenter", showhomebox);
+homemenu.addEventListener("mouseleave", hidehomebox);
 function showhomebox() {
-    homebox.style.display = 'block';
+  homebox.style.display = "block";
 }
 function hidehomebox() {
-    homebox.style.display = 'none';
+  homebox.style.display = "none";
 }
 
-const placebtn = document.getElementById('place');
-const placemenu = document.getElementById('placebox');
+const placebtn = document.getElementById("place");
+const placemenu = document.getElementById("placebox");
 
-placebtn.addEventListener('mouseenter', showplacebox);
-placebtn.addEventListener('mouseleave', hideplacebox);
-placemenu.addEventListener('mouseenter', showplacebox);
-placemenu.addEventListener('mouseleave', hideplacebox);
+placebtn.addEventListener("mouseenter", showplacebox);
+placebtn.addEventListener("mouseleave", hideplacebox);
+placemenu.addEventListener("mouseenter", showplacebox);
+placemenu.addEventListener("mouseleave", hideplacebox);
 function showplacebox() {
-    placebox.style.display = 'block';
+  placebox.style.display = "block";
 }
 function hideplacebox() {
-    placebox.style.display = 'none';
+  placebox.style.display = "none";
 }
 
-const hotelbtn = document.getElementById('hotel');
-const hotelmenu = document.getElementById('hotelbox');
+const hotelbtn = document.getElementById("hotel");
+const hotelmenu = document.getElementById("hotelbox");
 
-hotelbtn.addEventListener('mouseenter', showhotelbox);
-hotelbtn.addEventListener('mouseleave', hidehotelbox);
-hotelmenu.addEventListener('mouseenter', showhotelbox);
-hotelmenu.addEventListener('mouseleave', hidehotelbox);
+hotelbtn.addEventListener("mouseenter", showhotelbox);
+hotelbtn.addEventListener("mouseleave", hidehotelbox);
+hotelmenu.addEventListener("mouseenter", showhotelbox);
+hotelmenu.addEventListener("mouseleave", hidehotelbox);
 function showhotelbox() {
-    hotelbox.style.display = 'block';
+  hotelbox.style.display = "block";
 }
 function hidehotelbox() {
-    hotelbox.style.display = 'none';
+  hotelbox.style.display = "none";
 }
 
-const foodbtn = document.getElementById('food');
-const foodmenu = document.getElementById('foodbox');
+const foodbtn = document.getElementById("food");
+const foodmenu = document.getElementById("foodbox");
 
-foodbtn.addEventListener('mouseenter', showfoodbox);
-foodbtn.addEventListener('mouseleave', hidefoodbox);
-foodmenu.addEventListener('mouseenter', showfoodbox);
-foodmenu.addEventListener('mouseleave', hidefoodbox);
+foodbtn.addEventListener("mouseenter", showfoodbox);
+foodbtn.addEventListener("mouseleave", hidefoodbox);
+foodmenu.addEventListener("mouseenter", showfoodbox);
+foodmenu.addEventListener("mouseleave", hidefoodbox);
 function showfoodbox() {
-    foodbox.style.display = 'block';
+  foodbox.style.display = "block";
 }
 function hidefoodbox() {
-    foodbox.style.display = 'none';
+  foodbox.style.display = "none";
 }
